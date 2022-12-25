1a6084b7cd63ce09b3335caf79d9613ae49ad09c
MEmassine <116316715+ME-Massine@users.noreply.github.com>
2022-12-25 01:40:12 +0100

    Update depo_file.c

--- depo_file.c
+++ depo_file.c
@@ -1,51 +1,33 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-void check_depo()
+#include <conio.h>
+
+void check_bal()
 {
-    int depo, bal;
+    int bal, depo;
 
     FILE *fp;
-    fp = fopen("text files\\depo.txt", "w");
+
+    // ! reading balance value
+
+    fp = fopen("text files\\bal.txt", "r");
     if (fp == NULL)
     {
         printf("Unable to create file.\n");
         exit(EXIT_FAILURE);
     }
-    // ! getting depo value
-
-    printf("\nhow much do you want to deposit: ");
-    scanf("%d", &depo);
-    fprintf(fp, "%d", depo);
-
-    fclose(fp);
-
-    if (depo <= 0)
-    {
-        printf("invalid value");
-    }
-    fclose(fp);
-
-    // ! reading bal value
-    fp = fopen("text files\\bal.txt", "r");
     while (!feof(fp))
     {
         fscanf(fp, "%d", &bal);
     }
     fclose(fp);
 
-    bal = bal + depo;
-
-    // ! storing bal calue
+    // ? printing balance
+    printf("your balance is %d", bal);
     fp = fopen("text files\\bal.txt", "w");
     fprintf(fp, "%d", bal);
 
     fclose(fp);
 
-    // ! clearing depo value
-    fp = fopen("text files\\depo.txt", "w");
-    depo = 0;
-    fprintf(fp, "%d", depo);
-
-    fclose(fp);
 }
