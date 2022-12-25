0da8c06ebdf972b0341eee42af7ec36d215c7122
MEmassine <116316715+ME-Massine@users.noreply.github.com>
2022-12-25 01:40:00 +0100

    Update depo_file.c

--- depo_file.c
+++ depo_file.c
@@ -3,27 +3,49 @@
 #include <stdlib.h>
 void check_depo()
 {
-    int depo;
+    int depo, bal;
 
     FILE *fp;
     fp = fopen("text files\\depo.txt", "w");
     if (fp == NULL)
-
     {
         printf("Unable to create file.\n");
         exit(EXIT_FAILURE);
     }
-    if (depo > 0)
-    {
-        printf("\nhow much do you want to deposit: ");
-        scanf("%d", &depo);
-        fprintf(fp, "%d", depo);
-    }
+    // ! getting depo value
+
+    printf("\nhow much do you want to deposit: ");
+    scanf("%d", &depo);
+    fprintf(fp, "%d", depo);
+
+    fclose(fp);
 
     if (depo <= 0)
     {
         printf("invalid value");
     }
+    fclose(fp);
+
+    // ! reading bal value
+    fp = fopen("text files\\bal.txt", "r");
+    while (!feof(fp))
+    {
+        fscanf(fp, "%d", &bal);
+    }
+    fclose(fp);
+
+    bal = bal + depo;
+
+    // ! storing bal calue
+    fp = fopen("text files\\bal.txt", "w");
+    fprintf(fp, "%d", bal);
+
+    fclose(fp);
+
+    // ! clearing depo value
+    fp = fopen("text files\\depo.txt", "w");
+    depo = 0;
+    fprintf(fp, "%d", depo);
 
     fclose(fp);
 }
